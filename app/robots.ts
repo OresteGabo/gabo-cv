b59d1c44f9c2be18eb24016a1d2d42e7efdca602
oreste <muhirwa.g.oreste@gmail.com>
2026-06-14 02:30:27 +0200

    chore: remove unnecessary disallow rule from robots.txt configuration

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,6 @@ export default function robots(): MetadataRoute.Robots {
         rules: {
             userAgent: '*',
             allow: '/',
-            disallow: '/private/', // Exemple si tu as un dossier que tu veux cacher
         },
         sitemap: 'https://orestegabo.dev/sitemap.xml',
     }
