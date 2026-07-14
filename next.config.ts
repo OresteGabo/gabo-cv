1664f97b4b8de650d7223943945a954d08efbd2b
oreste <muhirwa.g.oreste@gmail.com>
2026-07-14 16:27:26 +0200

    chore: include bonds-portfolio.json in output file tracing configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  outputFileTracingIncludes: {
+    "/*": ["./.data/bonds-portfolio.json"],
+  },
   poweredByHeader: false,
   reactCompiler: true,
   async headers() {
