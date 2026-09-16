edf1b1fc36b468031f32ad78f9f2b64358bb16e3
oreste <muhirwa.g.oreste@gmail.com>
2026-09-16 21:42:28 +0200

    chore: remove hardcoded default email from BondIssuanceCalendar

--- component/bonds/BondIssuanceCalendar.tsx
+++ component/bonds/BondIssuanceCalendar.tsx
@@ -354,7 +354,6 @@ export function BondIssuanceCalendar() {
                 type="email"
                 required
                 autoComplete="username"
-                defaultValue="orestegabo@icloud.com"
                 className="mt-2 w-full rounded-xl border border-outline/10 bg-background px-4 py-3 text-on-surface outline-none focus:border-primary/60"
               />
             </label>
